8a3e70ada8e8eae609ee4e7c0693f9e17265f309
Tim van den Dobbelsteen <timvdd@hotmail.com>
2025-02-03 13:28:58 +0100

    Remaining agencies

---
+++ src/scrapers/househunting/scraper.ts
@@ -0,0 +1,2 @@
+export const url =
+  'https://househunting.nl/woningaanbod/?available-since=&m2=50%2B&type=for-rent&filter_location=Groningen&lat=53.2193835&lng=6.5665018&km=5&km=5&min-price=300&max-price=1500&vestiging=&sort=';
